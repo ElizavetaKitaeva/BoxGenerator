05d8e350f42a0865a11e6e5bbe57da6a3fe3cc90
Eilla <eilla@student.21-school.ru>
2023-10-06 17:11:46 +0700

    webpack conf"

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: './index_bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
